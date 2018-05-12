a5d1f3af0efd2da023876ceeb5df1bb429d9032b
Sakura Hanabi <a632079@gmail.com>
2018-05-12 14:34:13 +0800

    Fix Sync Problems. Auto fetch latest branch.

--- core.js
+++ core.js
@@ -1,10 +1,31 @@
 'use strict'
+// Import Packages
 const child = require('child_process')
 const util = require('util')
 const path = require('path')
+const fetch = require('node-fetch')
 const CronJob = require('cron').CronJob
 const pify = util.promisify
+
+// Init
 const exec = pify(child.exec)
+
+// Static Vars
+const github_api = 'https://api.github.com'
+const nodebb_tags = '/repos/NodeBB/NodeBB/tags'
+
+// fetch latest Target Branch
+async function fetchBranch() {
+  const tagsDataResponse = await fetch(`${github_api}${nodebb_tags}`)
+  const tagsData = await tagsDataResponse.json()
+  // console.log(tagsData)
+  const currentVersion = Array.isArray(tagsData) ? tagsData[0].name : null
+  if (!currentVersion) {
+    throw new Error('Can\'t fetch latest NodeBB Branch. ')
+  }
+  const Branch = currentVersion.slice(0, currentVersion.length - 2) + '.x'
+  return Branch
+}
 
 // Sync Repos
 async function syncDocs() {
@@ -48,13 +69,13 @@ async function syncNodeBB() {
   data = await execCMD('git push target')
   console.log(data)
 
-  data = await execCMD('git checkout v1.8.x')
+  const branch = await fetchBranch()
+  data = await execCMD(`git checkout ${branch}`)
   console.log(data)
   data = await execCMD('git pull')
   console.log(data)
   data = await execCMD('git push target')
   console.log(data)
-
 }
 
 async function sync() {
@@ -79,3 +100,4 @@ const job = new CronJob({
   timeZone: 'Asia/Shanghai'
 })
 job.start()
+
